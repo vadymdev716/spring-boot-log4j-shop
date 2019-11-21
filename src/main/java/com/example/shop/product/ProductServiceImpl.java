f23ca7a1d4c0973ce4e189ba0df5d821a355389f
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-21 15:06:56 +0200

    Updated class ProductServiceImpl

--- src/main/java/com/example/shop/product/ProductServiceImpl.java
+++ src/main/java/com/example/shop/product/ProductServiceImpl.java
@@ -1,23 +1,29 @@
 package com.example.shop.product;
 
+import com.example.shop.product.filter.ProductFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductServiceImpl implements ProductService {
     private ProductRepo productRepo;
+    private ProductFilter productFilter;
 
     @Autowired
-    public ProductServiceImpl(ProductRepo productRepo) {
+    public ProductServiceImpl(ProductRepo productRepo, ProductFilter productFilter) {
         this.productRepo = productRepo;
+        this.productFilter = productFilter;
     }
 
-    //100 or 1000
+    @Async
     @Override
-    public List<Product> getProductList(String condition) {
-        return productRepo.getProducts();
+    public CompletableFuture<List<Product>> getProductList(String condition) {
+        List<Product> products = productRepo.getProducts();
+        List<Product> filteredProducts = productFilter.nameRegularExpressionFilter(products, condition);
+        return CompletableFuture.completedFuture(filteredProducts);
     }
-
 }
