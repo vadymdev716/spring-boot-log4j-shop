20e9bcac5fac69fc4c8e2d73e537423ee91747aa
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-21 14:37:42 +0200

    Updated class Product

--- src/main/java/com/example/shop/product/Product.java
+++ src/main/java/com/example/shop/product/Product.java
@@ -1,31 +1,13 @@
 package com.example.shop.product;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
 public class Product {
     private int id;
     private String name;
     private String description;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
