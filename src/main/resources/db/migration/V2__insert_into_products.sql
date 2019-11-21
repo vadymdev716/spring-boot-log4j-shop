0debe2b55bdb6f99f7d84b2328e059c97811999a
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-21 15:34:15 +0200

    Updated V2__insert_into_products.sql (test data, for migration)

--- src/main/resources/db/migration/V2__insert_into_products.sql
+++ src/main/resources/db/migration/V2__insert_into_products.sql
@@ -1,16 +1,18 @@
-INSERT INTO products (name, description) VALUES ('name 1', 'description 1');
-INSERT INTO products (name, description) VALUES ('name 2', 'description 2');
-INSERT INTO products (name, description) VALUES ('name 3', 'description 3');
-INSERT INTO products (name, description) VALUES ('name 4', 'description 4');
-INSERT INTO products (name, description) VALUES ('name 5', 'description 5');
+INSERT INTO products (name, description) VALUES ('elena 1', 'description 1');
+INSERT INTO products (name, description) VALUES ('elena 2', 'description 2');
 
-INSERT INTO products (name, description) VALUES ('name 6', 'description 6');
-INSERT INTO products (name, description) VALUES ('name 7', 'description 7');
-INSERT INTO products (name, description) VALUES ('name 8', 'description 8');
-INSERT INTO products (name, description) VALUES ('name 9', 'description 9');
-INSERT INTO products (name, description) VALUES ('name 10', 'description 10');
+INSERT INTO products (name, description) VALUES ('Elena 3', 'description 3');
+INSERT INTO products (name, description) VALUES ('Elena 4', 'description 4');
 
+INSERT INTO products (name, description) VALUES ('Tom 5', 'description 5');
+INSERT INTO products (name, description) VALUES ('Tom 6', 'description 6');
 
+INSERT INTO products (name, description) VALUES ('nic 7', 'description 7');
+INSERT INTO products (name, description) VALUES ('nic 8', 'description 8');
+
+INSERT INTO products (name, description) VALUES ('Nica 9', 'description 9');
+INSERT INTO products (name, description) VALUES ('Nicv 10', 'description 10');
+INSERT INTO products (name, description) VALUES ('Nice 11', 'description 11');
 
 
 
