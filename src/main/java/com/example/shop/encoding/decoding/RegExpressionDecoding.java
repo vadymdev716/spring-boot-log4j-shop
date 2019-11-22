26495be57809c1241f52bf01a573fca693228137
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-22 21:29:05 +0200

    Added interface RegExpressionDecoding

---
+++ src/main/java/com/example/shop/encoding/decoding/RegExpressionDecoding.java
@@ -0,0 +1,9 @@
+package com.example.shop.encoding.decoding;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface RegExpressionDecoding {
+
+    String decode(String str);
+}
