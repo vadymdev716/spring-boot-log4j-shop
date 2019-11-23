41408566d9616a6e0aae138555b69b91247dc06d
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-23 23:56:57 +0200

    Added test class ProductServiceImplTest

---
+++ src/test/java/com/example/shop/product/ProductServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.example.shop.product;
+
+import com.example.shop.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ProductServiceImplTest {
+
+    @Test
+    public void getProductList() {
+    }
+}
