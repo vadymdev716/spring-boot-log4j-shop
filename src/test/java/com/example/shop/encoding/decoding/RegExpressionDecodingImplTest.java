dedb28398df222272b0c27af1dba273d5a08137d
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-23 23:09:54 +0200

    Added test class RegExpressionDecodingImplTest

---
+++ src/test/java/com/example/shop/encoding/decoding/RegExpressionDecodingImplTest.java
@@ -0,0 +1,23 @@
+package com.example.shop.encoding.decoding;
+
+import com.example.shop.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class RegExpressionDecodingImplTest {
+    @Autowired
+    RegExpressionDecoding regExpressionDecoding;
+
+    @Test
+    public void decode_valid_test() {
+        assertEquals("^E.*$", regExpressionDecoding.decode("XkUuKiQ="));
+    }
+}
