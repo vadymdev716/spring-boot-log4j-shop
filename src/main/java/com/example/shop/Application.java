3ad6f3a7fff786fa4648383cdac977782f1ea972
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-23 22:59:27 +0200

    Updated class Application

--- src/main/java/com/example/shop/Application.java
+++ src/main/java/com/example/shop/Application.java
@@ -3,6 +3,7 @@ package com.example.shop;
 import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Log4j
