628cffd73f6e9a0b6ae14135d0adf324f86fbc5b
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-23 23:48:11 +0200

    Updated class Product (Added @EqualsAndHashCode annotation + added constructor for test needs)

--- src/main/java/com/example/shop/product/Product.java
+++ src/main/java/com/example/shop/product/Product.java
@@ -1,13 +1,21 @@
 package com.example.shop.product;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@EqualsAndHashCode
 @NoArgsConstructor
 public class Product {
     private int id;
     private String name;
     private String description;
+
+    public Product(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }
