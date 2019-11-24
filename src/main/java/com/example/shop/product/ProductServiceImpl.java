b26487802473409b08daf19f01f2f60b3b532251
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-24 16:18:51 +0200

    Updated class ProductServiceImpl

--- src/main/java/com/example/shop/product/ProductServiceImpl.java
+++ src/main/java/com/example/shop/product/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.shop.product;
 
+import com.example.shop.encoding.decoding.RegExpressionDecoding;
 import com.example.shop.product.filter.ProductFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -12,18 +13,26 @@ import java.util.concurrent.CompletableFuture;
 public class ProductServiceImpl implements ProductService {
     private ProductRepo productRepo;
     private ProductFilter productFilter;
+    private RegExpressionDecoding regExpressionDecoding;
+    private ProductValidation productValidation;
 
     @Autowired
-    public ProductServiceImpl(ProductRepo productRepo, ProductFilter productFilter) {
+    public ProductServiceImpl(ProductRepo productRepo, ProductFilter productFilter,
+                              RegExpressionDecoding regExpressionDecoding, ProductValidation productValidation) {
         this.productRepo = productRepo;
         this.productFilter = productFilter;
+        this.regExpressionDecoding = regExpressionDecoding;
+        this.productValidation = productValidation;
     }
 
     @Async
     @Override
-    public CompletableFuture<List<Product>> getProductList(String condition) {
+    public CompletableFuture<List<Product>> getProductList(String filterCondition) {
+        productValidation.checkLength(filterCondition);
+
+        String filter = regExpressionDecoding.decode(filterCondition);
         List<Product> products = productRepo.getProducts();
-        List<Product> filteredProducts = productFilter.nameRegularExpressionFilter(products, condition);
+        List<Product> filteredProducts = productFilter.nameRegularExpressionFilter(products, filter);
         return CompletableFuture.completedFuture(filteredProducts);
     }
 }
