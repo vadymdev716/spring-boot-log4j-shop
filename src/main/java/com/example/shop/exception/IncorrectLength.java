c9004ca193fb28d44862af900f950b0c2eef73d4
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-24 16:20:02 +0200

    Updated class IncorrectLength

--- src/main/java/com/example/shop/exception/IncorrectLength.java
+++ src/main/java/com/example/shop/exception/IncorrectLength.java
@@ -1,5 +1,9 @@
 package com.example.shop.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Incorrect entered parameter length!")
 public class IncorrectLength extends RuntimeException {
 
     public IncorrectLength(String message) {
