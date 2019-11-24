15e07d7b4b64732c919395d12ed73bf1e57ad605
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-24 16:17:16 +0200

    Updated V2__insert_into_products.sql Added markers to understand that exactly our filter will not miss

--- src/main/resources/db/migration/V2__insert_into_products.sql
+++ src/main/resources/db/migration/V2__insert_into_products.sql
@@ -1,6 +1,8 @@
+--2nd
 INSERT INTO products (name, description) VALUES ('elena 1', 'description 1');
 INSERT INTO products (name, description) VALUES ('elena 2', 'description 2');
-
+--1th
+--2nd
 INSERT INTO products (name, description) VALUES ('Elena 3', 'description 3');
 INSERT INTO products (name, description) VALUES ('Elena 4', 'description 4');
 
@@ -9,7 +11,7 @@ INSERT INTO products (name, description) VALUES ('Tom 6', 'description 6');
 
 INSERT INTO products (name, description) VALUES ('nic 7', 'description 7');
 INSERT INTO products (name, description) VALUES ('nic 8', 'description 8');
-
+--2nd
 INSERT INTO products (name, description) VALUES ('Nica 9', 'description 9');
 INSERT INTO products (name, description) VALUES ('Nicv 10', 'description 10');
 INSERT INTO products (name, description) VALUES ('Nice 11', 'description 11');
