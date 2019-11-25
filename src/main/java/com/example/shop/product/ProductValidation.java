98f8c61a3f6e68d9106111277887648dc72965e1
vadymdev716 <vadymmartynenko0@gmail.com>
2019-11-25 03:29:21 +0200

    Updated class ProductValidation

--- src/main/java/com/example/shop/product/ProductValidation.java
+++ src/main/java/com/example/shop/product/ProductValidation.java
@@ -1,6 +1,6 @@
 package com.example.shop.product;
 
-import com.example.shop.exception.IncorrectLength;
+import com.example.shop.exception.IncorrectParam;
 import com.example.shop.exception.NotFoundException;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Component;
@@ -14,7 +14,7 @@ public class ProductValidation {
     public void checkLength(String filterCondition) {
         if (filterCondition.length() == 0) {
             log.error(String.format("Incorrect entered parameter length (filterCondition='%s')!", filterCondition));
-            throw new IncorrectLength("Incorrect entered parameter length!");
+            throw new IncorrectParam("Incorrect entered parameter length!");
         }
     }
 
